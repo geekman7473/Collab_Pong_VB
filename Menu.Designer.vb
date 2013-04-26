fbae0a69c0034c6109d1f3f8524b18795d207351
Amir Amhaz <namhaza4@outlook.com>
2013-04-26 19:20:22 -0400

    Whoa so this is how GitHub works

--- Menu.Designer.vb
+++ Menu.Designer.vb
@@ -24,6 +24,8 @@ Partial Class Menu
     Private Sub InitializeComponent()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
         Me.SuspendLayout()
         '
         'TextBox1
@@ -42,11 +44,32 @@ Partial Class Menu
         Me.Button1.Text = "Button1"
         Me.Button1.UseVisualStyleBackColor = True
         '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(828, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(72, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Paddle speed"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"})
+        Me.ComboBox1.Location = New System.Drawing.Point(831, 29)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(69, 21)
+        Me.ComboBox1.TabIndex = 4
+        Me.ComboBox1.Text = "5"
+        '
         'Menu
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(912, 716)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.TextBox1)
         Me.Name = "Menu"
@@ -57,5 +80,7 @@ Partial Class Menu
     End Sub
     Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
     Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
 
 End Class
--- Paddle.vb
+++ Paddle.vb
@@ -1,3 +1,21 @@
 ﻿Public Class Paddle
-
+    <System.Runtime.InteropServices.DllImport("User32.dll")>
+    Private Shared Function GetAsyncKeyState(ByVal vkey As System.Windows.Forms.Keys) As Short
+    End Function
+    Public speed, width, height, x, y As Integer
+    Private pencil As Graphics
+    Public Sub movement(ByVal height As Integer, ByVal length As Integer)
+        speed = Menu.ComboBox1.Text
+        If GetAsyncKeyState(Keys.Up) And y > 0 Then
+            y -= speed
+        End If
+        If GetAsyncKeyState(Keys.Down) And y + 50 < height Then
+            x += speed
+        End If
+    End Sub
+    Public Sub drawpaddle(ByVal height As Integer, ByVal length As Integer)
+        width = length \ 10
+        height = length \ 5
+        pencil.FillRectangle(Brushes.Black, x, y, width, height)
+    End Sub
 End Class
